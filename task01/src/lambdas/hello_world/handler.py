125e7a16e5aa37ff41eb6f5ee3336c51b941d49d
shuraosipov <shuraosipov@gmail.com>
2024-04-25 15:26:08 -0400

    new message

--- task01/src/lambdas/hello_world/handler.py
+++ task01/src/lambdas/hello_world/handler.py
@@ -14,7 +14,7 @@ class HelloWorld(AbstractLambda):
         Explain incoming event here
         """
         # todo implement business logic
-        return 200
+        return {'statusCode': 200, 'message': 'Hello from Lambda'}
     
 
 HANDLER = HelloWorld()
